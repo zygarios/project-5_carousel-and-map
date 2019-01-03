18c5349435682d97c07f5522d494e2f8525dc16e
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2019-01-03 01:59:19 +0100

    Finish carousel

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-var flkty = new Flickity( '.carousel', {
+var flkty = new Flickity('.carousel', {
   hash: true,
+
+});
+
+
+
+var toggleButton = document.querySelector('.button--toggle');
+toggleButton.addEventListener('click', function() {
+  flkty.select(0);
+});
+
+var progressBar = document.querySelector('.progress-bar');
+
+flkty.on('scroll', function(progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + '%';
 });
