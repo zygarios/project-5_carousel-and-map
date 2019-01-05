5bcaed099bd592717f8767c94eea9f6345f4c708
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2019-01-05 13:51:05 +0100

    Add rwd carousel

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-var flkty = new Flickity('.carousel', {
-  hash: true,
+  var flkty = new Flickity('.carousel', {
+    hash: true,
+  });
 
-});
+  var toggleButton = document.querySelector('.button--toggle');
+  toggleButton.addEventListener('click', function() {
+    flkty.select(0);
+  });
 
+  var progressBar = document.querySelector('.progress-bar');
 
-
-var toggleButton = document.querySelector('.button--toggle');
-toggleButton.addEventListener('click', function() {
-  flkty.select(0);
-});
-
-var progressBar = document.querySelector('.progress-bar');
-
-flkty.on('scroll', function(progress) {
-  progress = Math.max(0, Math.min(1, progress));
-  progressBar.style.width = progress * 100 + '%';
-});
+  flkty.on('scroll', function(progress) {
+    progress = Math.max(0, Math.min(1, progress));
+    progressBar.style.width = progress * 100 + '%';
+  });
