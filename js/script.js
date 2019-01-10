b829e862970c02b799a44fb308840f371627c82b
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2019-01-10 20:12:12 +0100

    Add Gmaps

--- js/script.js
+++ js/script.js
@@ -1,28 +1,16 @@
-  var flkty = new Flickity('.carousel', {
-    hash: true,
-  });
+'use strict';
+(function(){
 
-  var toggleButton = document.querySelector('.button--toggle');
-  toggleButton.addEventListener('click', function() {
-    flkty.select(0);
-  });
 
-  var progressBar = document.querySelector('.progress-bar');
-
-  flkty.on('scroll', function(progress) {
-    progress = Math.max(0, Math.min(1, progress));
-    progressBar.style.width = progress * 100 + '%';
-  });
-
-var templateSlide = document.querySelector('#template').innerHTML;
-
-Mustache.parse(templateSlide);
-
-var allSlides = '';
-
-for (var i = 0; i <slides.length; i++) {
-  allSlides += Mustache.render(templateSlide, slides[i]);
+// Initialize and add the map
+window.initMap = function() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
 }
 
-var carouselSlides = document.querySelector('.carousel');
-carouselSlides.insertAdjacentHTML('beforeend', allSlides);
+})();
