f69ff86292fd0b84cdaf3c0a5f4a881a01cb0c1a
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2019-01-10 21:51:01 +0100

    Mustache fix

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+var templateSlide = document.querySelector('#template').innerHTML;
+
+Mustache.parse(templateSlide);
+
+var allSlides = '';
+
+for (var i = 0; i <slides.length; i++) {
+  allSlides += Mustache.render(templateSlide, slides[i]);
+}
+
+var carouselSlides = document.querySelector('.carousel');
+
+carouselSlides.insertAdjacentHTML('beforeend', allSlides);
+
   var flkty = new Flickity('.carousel', {
     hash: true,
   });
@@ -13,16 +27,3 @@
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
   });
-
-var templateSlide = document.querySelector('#template').innerHTML;
-
-Mustache.parse(templateSlide);
-
-var allSlides = '';
-
-for (var i = 0; i <slides.length; i++) {
-  allSlides += Mustache.render(templateSlide, slides[i]);
-}
-
-var carouselSlides = document.querySelector('.carousel');
-carouselSlides.insertAdjacentHTML('beforeend', allSlides);
