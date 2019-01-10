fab62d7b5a288ffd2045aba97150c2c3f320a59b
Bartosz Zygmunt <bartekzygmunt92@gmail.com>
2019-01-10 06:58:46 +0100

    Problem mustache

--- js/script.js
+++ js/script.js
@@ -13,3 +13,16 @@
     progress = Math.max(0, Math.min(1, progress));
     progressBar.style.width = progress * 100 + '%';
   });
+
+var templateSlide = document.querySelector('#template').innerHTML;
+
+Mustache.parse(templateSlide);
+
+var allSlides = '';
+
+for (var i = 0; i <slides.length; i++) {
+  allSlides += Mustache.render(templateSlide, slides[i]);
+}
+
+var carouselSlides = document.querySelector('.carousel');
+carouselSlides.insertAdjacentHTML('beforeend', allSlides);
